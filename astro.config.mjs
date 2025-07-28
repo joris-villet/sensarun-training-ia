5ff1bc9119e764c302c33942fb4d307c395c92d0
joris villet <julek.krakow@proton.me>
2025-07-28 17:11:34 +0200

    add: plugin cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import node from '@astrojs/node';
-
 import svelte from '@astrojs/svelte';
-
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  }),
-
   output: 'server',
   integrations: [svelte()],
-
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: cloudflare({
+    imageService: 'compile'
+  })
 });
